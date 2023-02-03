0e5e303277bf37c55b9fa8eac339700c49f9cfef
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-02-02 22:15:54 -0600

    Fucion para la resta

---
+++ tarea03/funcion_resta.py
@@ -0,0 +1,6 @@
+def resta():
+    resta_uno = int(input("Ingrese el primer Numero: "))
+    resta_dos = int(input("Ingrese el segundo Numero: "))
+    total = resta_uno - resta_dos
+    print(f"La resta de ambos valores es: {total}.")
+resta()
