2c9b713c530cede4af2be7bab61148a6d6c0b461
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-02-05 17:14:04 -0600

    Cree la funcion para la division

---
+++ tarea03/funcion_division.py
@@ -0,0 +1,14 @@
+# Esta es una funcion para dividir dos numeros ingresados por un usuario
+# El usuario debe ingresar dos numeros
+# El resultado de la division se guarda en el file "output_file_division.txt"
+
+def division():
+    '''Funcion para Dividir'''
+    primer_numero = int(input("Ingrese el primer numero que desea dividir: "))
+    segundo_numero = int(input(f"Ingrese el segundo numero que desea dividir: "))
+    total = primer_numero // segundo_numero
+    print(f"La division entre ambos numeros es: {total}.")
+    output_file = open("output_file_division.txt", "a") # Open/create output file in append mode
+    output_file.write(f"El resultado de la division de {primer_numero} // {segundo_numero} es: {total}\n") # Appends the results to an output file
+    output_file.close() # Closes the file
+division()
