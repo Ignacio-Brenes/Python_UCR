b3c0b1e0ea7b8891d1dfa0ab23c5b87558f8fea3
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-02-05 21:31:40 -0600

    Agregue recursividad a la funcion

--- tarea03/funcion_resta.py
+++ tarea03/funcion_resta.py
@@ -1,14 +1,26 @@
+'''Restar'''
 # Esta es una funcion para restar numeros ingresados por un usuario
 # El usuario debe ingresar dos numeros
-# El resultado de la suma se guarda en el file "output_file_suma.txt"
+# El resultado de la suma se guarda en el file "output_file_resta.txt"
 
 def resta():
     '''Funcion para Restar'''
-    resta_uno = int(input("Ingrese el primer Numero: "))
-    resta_dos = int(input("Ingrese el segundo Numero: "))
-    total = resta_uno - resta_dos
-    print(f"La resta de ambos valores es: {total}.")
-    output_file = open("output_file_resta.txt", "a") # Open/create output file in append mode
-    output_file.write(f"La resta total de {resta_uno} - {resta_dos} es: {total}\n") # Appends the results to an output file
-    output_file.close() # Closes the file
-# resta()
+# Recursividad
+    while True:
+        resta_uno = int(input("Ingrese el primer Numero: "))
+        resta_dos = int(input("Ingrese el segundo Numero: "))
+        total = resta_uno - resta_dos
+        print(f"El resultado de la resta de ambos valores es: {total}.")
+        output_file = open("output_file_resta.txt", "a") # Open/create output file in append mode
+        output_file.write(f"El resultado de resta total de {resta_uno} - {resta_dos} es: {total}\n") # Appends the results to an output file
+        output_file.close() # Closes the file
+
+# Preguntar al usuario si desea continua
+        continuar = str(input("Indique (Y/N) para realizar otra resta: "))
+        continuar = continuar.upper() 
+        if continuar == "N":
+            print("Gracias!!!")
+            break
+        else:
+            continue
+resta()
