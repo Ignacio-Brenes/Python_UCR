17b4171b654db5456fc0b2d25ea916d845d1fa59
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-02-05 22:47:51 -0600

    Agregue recursividad a la funcion

--- tarea03/funcion_division.py
+++ tarea03/funcion_division.py
@@ -4,11 +4,18 @@
 
 def division():
     '''Funcion para Dividir'''
-    primer_numero = int(input("Ingrese el primer numero que desea dividir: "))
-    segundo_numero = int(input(f"Ingrese el segundo numero que desea dividir: "))
-    total = primer_numero // segundo_numero
-    print(f"La division entre ambos numeros es: {total}.")
-    output_file = open("output_file_division.txt", "a") # Open/create output file in append mode
-    output_file.write(f"El resultado de la division de {primer_numero} // {segundo_numero} es: {total}\n") # Appends the results to an output file
-    output_file.close() # Closes the file
+    while True:
+        primer_numero = int(input("Ingrese el primer numero que desea dividir: "))
+        segundo_numero = int(input(f"Ingrese el segundo numero que desea dividir: "))
+        total = primer_numero // segundo_numero
+        print(f"La division entre ambos numeros es: {total}")
+        output_file = open("output_file_division.txt", "a") # Open/create output file in append mode
+        output_file.write(f"El resultado de la division de {primer_numero} // {segundo_numero} es: {total}\n") # Appends the results to an output file
+        output_file.close() # Closes the file
+        # Preguntar al usuario si desea continuar
+        continuar = str(input("Indique (Y/N) para continuar: ")) 
+        continuar = continuar.upper()
+        if continuar == "N":
+            print("\nGracias!")
+            break
 division()
