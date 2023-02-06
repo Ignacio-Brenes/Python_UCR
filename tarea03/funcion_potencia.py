269c182202c3ad7eaa4080de9961e653265b5746
SpaceParrot21 <122844987+SpaceParrot21@users.noreply.github.com>
2023-02-05 22:35:18 -0600

    funcion potencia con recursividad y guardar output a un file

---
+++ tarea03/funcion_potencia.py
@@ -0,0 +1,22 @@
+'''Potencia'''
+
+# Esta es una funcion para obtner la potencia de un numero elevado a otro.
+# El resultado de la operecion se guarda en el file "output_file_potencia.txt".
+def potencia():
+    while True:
+        base = int(input("Ingrese el numero base: "))
+        exponte = int(input("Ingrese el numero exponente: "))
+        # Funcion pow() para obtener la potencia de un numero
+        potencia = pow(base, exponte) 
+        print(f"El numero {base} elevado al {exponte} es: {potencia}" )
+        # Crear file, escribir resultado y cerrar 
+        output_file = open("output_file_potencia.txt", "a")
+        output_file.write(f"\nEl numero {base} elevado al {exponte} es: {potencia}\n")
+        output_file.close()
+        # Preguntar al usuario si desea continuar
+        continuar = str(input("Indique (Y/N) para continuar: ")) 
+        continuar = continuar.upper()
+        if continuar == "N":
+            print("\nGracias!")
+            break
+potencia()
